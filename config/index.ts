f7b08f891c5576120f504d7ac9854551d876d1ec
Porlar <wkkn2580@gmail.com>
2022-02-16 09:52:07 +0630

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -2,4 +2,4 @@ const dev = process.env.NODE_ENV !== "production";
 
 export const baseURL = dev
   ? "http://localhost:3000"
-  : "https://your_deployment.server.com";
+  : "https://rms-admin.vercel.app/";
